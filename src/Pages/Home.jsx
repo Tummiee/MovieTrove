95d5806a49f344b3a04940648edfaa2a33bb3332
Your Name <tunmiseelisha98@gmail.com>
2024-08-10 08:19:16 +0100

    ...

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,14 +2,11 @@ import React from 'react'
 import '../Styles/Home.css'
 import Badboys from '../assets/Badboys.jpg'
 import { GoDotFill } from "react-icons/go";
-import { MdStarRate } from "react-icons/md";
-import { MdOutlineStarRate } from "react-icons/md";
 import { FaRegHeart } from "react-icons/fa";
 import { MdOutlineFileDownload } from "react-icons/md";
 import { IoChatbubblesOutline } from "react-icons/io5";
-import { FaStarHalfAlt } from "react-icons/fa";
 import StarRating from '../components/StarRating';
-
+import { IoIosSearch } from "react-icons/io";
 
 const Home = () => {
 
@@ -44,6 +41,17 @@ const Home = () => {
                         <p><span><IoChatbubblesOutline /></span>21</p>
                         <p><span><MdOutlineFileDownload /></span>564</p>
                 </div>
+            </div>
+        </div>
+
+        <div className="lower-home-page">
+            <div className="mini-nav">
+                <h3>FEATURED MOVIES</h3>
+                <h4><i>New Released <span>/ Trailers & Clips</span></i></h4>
+                <form className='search'>
+                    <input type="text" placeholder='search'></input>
+                    <button type='button' className='btn'><IoIosSearch /></button>
+                </form>
             </div>
         </div>
     </div>
